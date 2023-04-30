4dd4411a64956163db3350da80bc3272a4b1cb32
Alex <asu4net@gmail.com>
2023-04-30 16:28:26 +0200

    Update Game.cpp

--- Engine/Nit/Source/Core/Game.cpp
+++ Engine/Nit/Source/Core/Game.cpp
@@ -1,6 +1,4 @@
 #include "Game.h"
-#include "Asset.h"
-#include "Serialization.h"
 #include "Rendering/Renderer2D.h"
 #include "Window/Window.h"
 
@@ -11,32 +9,9 @@ namespace Nit
         , m_Renderer2D(Renderer2D::CreateSingleton())
         , m_LayerStack(std::make_shared<LayerStack>())
     {}
-
-    class Dummy
-    {
-    public:
-        Dummy() = default;
-        std::string variable;
-        RTTR_ENABLE();
-    };
-    
-    RTTR_REGISTRATION
-    {
-        rttr::registration::class_<Dummy>("Dummy")
-            .constructor<>()
-            .constructor<Dummy&>()
-            .property("variable", &Dummy::variable);
-    }
     
     void Game::Initialize()
     {
-        Asset asset{"alex", "jaja", Id()};
-        const std::string result = Serialization::ToJson(asset);
-        std::cout << result << std::endl;
-        
-        Asset copyAsset;
-        Serialization::FromJson(result, copyAsset);
-        
         m_Window->Initialize();
         
         m_Renderer2D.Initialize(m_Window);
