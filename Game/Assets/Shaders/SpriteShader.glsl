9c71a6d25fe589ef7ccea229c5519b530252cabd
Alex <asu4net@gmail.com>
2024-03-15 18:19:38 +0100

    Update SpriteShader.glsl

--- Game/Assets/Shaders/SpriteShader.glsl
+++ Game/Assets/Shaders/SpriteShader.glsl
@@ -2,8 +2,8 @@
 #version 410 core
 
 layout(location = 0)  vec3 a_Position;
-layout(location = 1)  vec4 a_Color;
-layout(location = 2)  vec2 a_UV;
+layout(location = 1)  vec4 a_TintColor;
+layout(location = 2)  vec2 a_UVCoords;
 layout(location = 3)  int  a_TextureSlot;
 layout(location = 4)  int  a_EntityID;
 
@@ -11,8 +11,8 @@ uniform mat4 u_ProjectionViewMatrix;
 
 struct VertexOutput
 {
-    vec4 Color;
-    vec2 UV;
+    vec4 TintColor
+    vec2 UVCoords
     int  TextureSlot;
     int  EntityID;
 }
@@ -24,8 +24,8 @@ void main()
 {
     gl_Position    = u_ProjectionViewMatrix * vec4(a_Position, 1.0);
     
-    vo.Color       = a_Color;
-    vo.UV          = a_UV;
+    vo.TintColor   = a_TintColor;
+    vo.UVCoords    = a_UVCoords;
     vo.TextureSlot = a_TextureSlot;
     vo.EntityID    = a_EntityID;
 }
@@ -44,5 +44,5 @@ uniform sampler2D u_TextureSlots[32];
 void main()
 {
     EntityID = vi.EntityID;
-    FragColor = texture(u_TextureSlots[vi.TextureSlot], vi.UV) * vi.Color;       
+    FragColor = texture(u_TextureSlots[vi.TextureSlot], vi.UVCoords * vi.TintColor);       
 }
